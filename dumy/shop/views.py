da16feba1151a697ea71072ada5ecf982beedb69
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-03-06 17:13:07 +0500

    Exercised Previous Learned Material

---
+++ dumy/shop/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Home Page <button><a href='http://127.0.0.1:8000/shop/about/'>About</a></button><button><a href='http://127.0.0.1:8000/shop/contact/'>Contact</a></button><button><a href='http://127.0.0.1:8000/shop/blog/'>blog</a></button>")
+
+def about(request):
+    return HttpResponse("about Page <button><a href='http://127.0.0.1:8000/shop/home/'>Home</a></button><button><a href='http://127.0.0.1:8000/shop/contact/'>Contact</a></button><button><a href='http://127.0.0.1:8000/shop/blog/'>blog</a></button>")
+
+def contact(request):
+    return HttpResponse("contact Page <button><a href='http://127.0.0.1:8000/shop/home/'>Home</a></button><button><a href='http://127.0.0.1:8000/shop/about/'>About</a></button><button><a href='http://127.0.0.1:8000/shop/blog/'>blog</a></button>")
+
+def blog(request):
+    return HttpResponse("blog Page <button><a href='http://127.0.0.1:8000/shop/home/'>Home</a></button><button><a href='http://127.0.0.1:8000/shop/about/'>About</a></button><button><a href='http://127.0.0.1:8000/shop/Contact/'>Contact</a></button>")
+
+
+# Create your views here.
