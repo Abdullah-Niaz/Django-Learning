d8be9f76a1b5fe0850f87e9461e466ba769d48a9
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-06-02 01:43:27 +0500

    Django Models

--- 18_Day/ecomerce/views.py
+++ 18_Day/ecomerce/views.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from django.shortcuts import render
 
+# importing models 
+from shop.models import *
+
+
 def defaulthome(request):
-    return render(request,'core.html')
+    stu = student.objects.all().values()
+    return render(request,'core.html',{'stu':stu})
+    return render
--- 18_Day/shop/models.py
+++ 18_Day/shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class student(models.Model):
+    stuid = models.IntegerField()
+    stuName =  models.CharField(max_length=70)
+    stuAddress = models.CharField(max_length=70)
+    stuMessage = models.CharField( max_length=50,default="NAN")
