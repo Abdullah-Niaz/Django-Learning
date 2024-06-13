ebaa9e6a6b442fbc0f9b63850d0ef29cd3f79a75
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-06-13 16:27:56 +0500

    Form Validations

--- 22_Day/shop/forms.py
+++ 22_Day/shop/forms.py
@@ -3,43 +3,36 @@ from django import forms
 from django.core import validators
 
 class ShopForm(forms.Form):
-    name = forms.CharField(validators=[validators.MaxLengthValidator(10)])
-    email = forms.EmailField(required=True)
-    password = forms.CharField(widget=forms.PasswordInput())
-    rpassword = forms.CharField(widget=forms.PasswordInput())
+    error_css_class = 'error'
+    required_css_class = 'required'
+    name = forms.CharField(validators=[validators.MaxLengthValidator(10)] ,error_messages={'required':"Enter the Name!"})
+    email = forms.EmailField(required=True, error_messages={'required':"Enter the Email!"})
+    password = forms.CharField(widget=forms.PasswordInput(), error_messages={'required':"Enter the Password!"})
+    rpassword = forms.CharField(widget=forms.PasswordInput(), error_messages={'required':"Enter the Password!"})
 
     # check = forms.BooleanField(required=True)
     # file = forms.FileField()
     def clean(self):
         cleaned_data = super().clean()
-        valName = self.cleaned_data['name']
-        if (len(valName) < 5):
-            raise forms.ValidationError("Enter the Name More than 5 Charc")
-        val_email = self.cleaned_data.get('email')
-        if val_email is None:
-            raise forms.ValidationError("Email is required.")
-        if len(val_email) < 12:
-            raise forms.ValidationError("Enter an email with more than 12 characters.")   
-
-
-        val_pass = self.cleaned_data['password']
-        rval_pass = self.cleaned_data['rpassword']
+        val_pass = self.cleaned_data.get('password')
+        rval_pass = self.cleaned_data.get('rpassword')
+        # rval_pass = self.cleaned_data['rpassword']
         if(val_pass != rval_pass):
             raise forms.ValidationError("Password Does not Match")
         
 
-    # def clean_name(self):
-    #     cleaned_data = super().clean()
-    #     valName = self.cleaned_data['name']
-    #     if (len(valName) < 5):
-    #         raise forms.ValidationError("Enter the Name More than 5 Charc")
-    #     return valName
-    # def clean_email(self):
-    #     val_email = self.cleaned_data.get('email')
-    #     if val_email is None:
-    #         raise forms.ValidationError("Email is required.")
-    #     if len(val_email) < 12:
-    #         raise forms.ValidationError("Enter an email with more than 12 characters.")
+    def clean_name(self):
+        cleaned_data = super().clean()
+        valName = self.cleaned_data['name']
+        if (len(valName) < 5):
+            raise forms.ValidationError("Enter the Name More than 5 Charc")
+        return valName
+    def clean_email(self):
+        val_email = self.cleaned_data.get('email')
+        if val_email is None:
+            raise forms.ValidationError("Email is required.")
+        if len(val_email) < 12:
+            raise forms.ValidationError("Enter an email with more than 12 characters.")
     #     return val_email
     
 
