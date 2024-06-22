0874a3535c3080acb2812dff50b7c1a08e072082
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-06-22 23:45:51 +0500

    added templates and bussines login

--- 28_Day/shop/views.py
+++ 28_Day/shop/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.contrib import messages
+from .forms import *
 # Create your views here.
+
+
+def registration(request):
+    if request.method == 'POST':
+        fm = Registration(request.POST)
+        if(fm.is_valid()):
+            fm.save()
+            messages = (request,messages.SUCCESS,'Ho Gea Bahi Submit')
+    else:
+        fm = Registration()
+    return render(request, "shop/regestration.html",{'form':fm})
