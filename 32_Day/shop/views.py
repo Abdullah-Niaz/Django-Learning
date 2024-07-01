aa95d408b99631661b2e8ef6ede25028a9efb786
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-01 22:23:20 +0500

    session methods

--- 32_Day/shop/views.py
+++ 32_Day/shop/views.py
@@ -7,18 +7,19 @@ def setsession(request):
     request.session['age'] = '21'
     return render(request,"setsession.html")
 def getsession(request):
-    # name = request.session['name']
-    # age = request.session['age']
-    name = request.session.get("nam",default="Guest")
-    age = request.session.get('ag',default="None")
-    return render(request,"getsession.html",{'name':name,'age':age})
+    name = request.session.get("name",default="Guest")
+    age = request.session.get('age',default="None")
+    cl = request.session.setdefault("Phone","+96")
+    items = request.session.items()
+    keys = request.session.keys()
+    return render(request,"getsession.html",{'name':name,'age':age,"keys":keys,"items":items,"default":cl})
 
 def delsession(request):
-    if 'name' and 'age' in request.session:
-        del request.session['name']
-        del request.session['age']
-    else:
-        f  = "No Cookies avaiable to delele"
-    
-    return render(request,"deletesession.html", {"f":f})
+    # if 'name' and 'age' in request.session:
+    #     del request.session['name']
+    #     del request.session['age']
+    # else:
+    #     f  = "No Cookies avaiable to delele"
+    request.session.flush()
+    return render(request,"deletesession.html")
     # return render(request, "deletesession.html")
