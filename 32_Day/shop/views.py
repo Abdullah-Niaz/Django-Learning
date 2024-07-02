dc9209c84e59dccf47547463bc8a7bbcfe34f1f5
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-02 22:47:22 +0500

    session methods

--- 32_Day/shop/views.py
+++ 32_Day/shop/views.py
@@ -3,10 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 
 def setsession(request):
+    request.session.set_expiry(0)
+    SESSION_EXPIRE_AT_BROWSER_CLOSE = True
     request.session['name'] = 'Rahul'
     request.session['age'] = '21'
     return render(request,"setsession.html")
 def getsession(request):
+    age = request.session.get_expiry_age()
+    print(age/12)
+    print(request.session.get_expiry_date())
+    print(request.session.get_session_cookie_age())
+    print(request.session.get_expire_at_browser_close())
+
     name = request.session.get("name",default="Guest")
     age = request.session.get('age',default="None")
     cl = request.session.setdefault("Phone","+96")
